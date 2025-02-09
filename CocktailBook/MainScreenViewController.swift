94f620721f8cf6d530e796672f6eaf5874de0f51
Yogesh Bhatt <yogesh774@gmail.com>
2025-02-09 16:05:45 +0530

    changes

--- CocktailBook/MainScreenViewController.swift
+++ CocktailBook/MainScreenViewController.swift
@@ -8,15 +8,13 @@ class MainScreenViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        if let scrollView = UIHostingController(rootView: CocktailListView()).view {
-            //let scrollView = UIScrollView()
-            view.addSubview(scrollView)
-            
-            scrollView.translatesAutoresizingMaskIntoConstraints = false
-            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        if let cocktailListView = UIHostingController(rootView: CocktailListView()).view {
+            view.addSubview(cocktailListView)
+            cocktailListView.translatesAutoresizingMaskIntoConstraints = false
+            cocktailListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+            cocktailListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+            cocktailListView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            cocktailListView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         }
     }
 }
